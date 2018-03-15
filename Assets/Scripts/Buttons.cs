f7f948e6b7c2da4616a9962e8a554ef6e36cbaae
Unknown <ugurtufekci.tobbetu@gmail.com>
2018-03-15 04:04:37 +0300

    Infinite scroll view added. v1.2
    There are some bugs in content field. (ie: content layer position changes after restart the game.so, buttons cant be rendered.)

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Buttons : MonoBehaviour
+{
+
+    public Button buttonComponent;
+    public Text nameLabel;
+  
+    private ScrollList scrollList;
+    
+    public void Setup(ScrollList currentScrollList)
+    {
+      
+        scrollList = currentScrollList;
+
+    }
+
+}
---
+++ Assets/Scripts/ScrollList.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class ScrollList : MonoBehaviour
+{
+     public Transform contentPanel;
+     public ButtonObjectPool buttonObjectPool;
+
+    void Start()
+    {
+        AddButtons();
+    }
+
+   
+    private void AddButtons()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+           
+            GameObject newButton = buttonObjectPool.GetObject();
+            newButton.transform.SetParent(contentPanel);
+
+            Buttons sampleButton = newButton.GetComponent<Buttons>();
+          
+            sampleButton.Setup(this);
+        }
+    }
+
+   
+}
